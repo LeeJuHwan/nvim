00d9889b88098400c7bb0f8363a4c9c40c2a5562
dave.dev <dave@glitycorp.com>
2026-07-10 11:05:25 +0900

    Enable line wrap in telescope preview

--- lua/vsy/telescope.lua
+++ lua/vsy/telescope.lua
@@ -57,4 +57,11 @@ telescope.setup({
 		},
 	},
 })
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TelescopePreviewerLoaded",
+	callback = function()
+		vim.wo.wrap = true
+	end,
+})
 --telescope.load_extension("projects")
